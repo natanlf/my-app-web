7fabfa43308203dd2e8b5b67b85f6fa4c8f61d2d
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2018-11-18 20:12:12 -0200

    Mostrando categorias

--- src/app/components/categoria-list/categoria-list.component.ts
+++ src/app/components/categoria-list/categoria-list.component.ts
@@ -1,5 +1,7 @@
+import { CategoriaDTO } from './../../model/categoria.dto';
 import { CategoriaService } from './../../services/categoria/categoria.service';
 import { Component, OnInit } from '@angular/core';
+import { API_CONFIG } from '../../services/config/api.config';
 
 @Component({
   selector: 'app-categoria-list',
@@ -8,12 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriaListComponent implements OnInit {
 
+  itens: CategoriaDTO[];
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+
   constructor(public categoriaService: CategoriaService) { }
 
   ngOnInit() {
+    this.findAll();
+  }
+
+  findAll(){
     this.categoriaService.findAll()
       .subscribe(response =>{ //com arrow function, declarei uma função dentro de outra
-        console.log(response); //tenho duas funções dentro da função subscribe, a response e a error
+        this.itens = response;
       },
     error => {
       console.log(error);
--- src/app/services/config/api.config.ts
+++ src/app/services/config/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
     //url base do web service
-    baseUrl : "http://localhost:8080"
+    baseUrl : "http://localhost:8080",
+    bucketBaseUrl : "https://s3-sa-east-1.amazonaws.com/natanlf-curso-spring-ionic"
 } 
